846d8c981c13f0285e00d32fc18e9d0720438d36
Vicente Arce <vicente.arce.p@usach.cl>
2024-12-14 18:26:53 -0300

    Implementando controlador almacen_producto

--- backend/src/main/java/proyecto/repositories/Almacen_ProductoRepository.java
+++ backend/src/main/java/proyecto/repositories/Almacen_ProductoRepository.java
@@ -9,7 +9,7 @@ public interface Almacen_ProductoRepository {
     ResponseEntity<List<Object>> findAll();
     ResponseEntity<Object> findById(int id_almacen_producto);
     ResponseEntity<Object> create(Almacen_ProductoEntity almacen_producto);
-    ResponseEntity<Object> update(Object almacen_producto);
+    ResponseEntity<Object> update(Almacen_ProductoEntity almacen_producto);
     ResponseEntity<Object> delete(int id_almacen_producto);
 
 }
