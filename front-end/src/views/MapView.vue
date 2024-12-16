799de86ab76fab6ced89e205db2a6784365e7c69
Vicente Arce <vicente.arce.p@usach.cl>
2024-12-16 14:28:59 -0300

    Solucionando Bug en MapView

--- front-end/src/views/MapView.vue
+++ front-end/src/views/MapView.vue
@@ -24,45 +24,45 @@
         <Button @click="fetchMidpoint">Calcular Punto Medio</Button>
       </div>
     </div>
-		<div>
-			<h2>Busca los repartidores por zona:</h2>
-			<label for="client1">Zonas:</label>
-        <select v-model="zonaSeleccionada" id="client1">
-          <option disabled value="">Selecciona una zona</option>
-          <option v-for="(zona, index) in zonas" :key="index" :value="zona">
-            - {{ zona.nombre }}
-          </option>
-        </select>
-				
-				<ButtonGroup>
-					<Button @click="mostrarZona(zonaSeleccionada)">Ver zona</Button>
-					<Button @click="buscarRepartidores">Ver repartidores</Button>
-					<Button @click="limpiarPoligono">Limpiar zona</Button>
-				</ButtonGroup>
+    <div>
+      <h2>Busca los repartidores por zona:</h2>
+      <label for="client1">Zonas:</label>
+      <select v-model="zonaSeleccionada" id="client1">
+        <option disabled value="">Selecciona una zona</option>
+        <option v-for="(zona, index) in zonas" :key="index" :value="zona">
+          - {{ zona.nombre }}
+        </option>
+      </select>
 
-				<br>
-				<br>
-				<h2>Crear zona:</h2>
-				<label for="nombreZona">Nombre:</label>
-				<input type="text" v-model="nombreZona" style="margin-right: 10px; height: 35px;"> </input>
-				<ButtonGroup>
-					<span v-if=" 3 <puntos.length ">
-						<Button @click="crearPoligono">Mostrar poligono</Button>
-						<Button @click="crearZona">Crear zona</Button>				
-					</span>
-				</ButtonGroup>
-		</div>
-		<br>
+      <ButtonGroup>
+        <Button @click="mostrarZona(zonaSeleccionada)">Ver zona</Button>
+        <Button @click="buscarRepartidores">Ver repartidores</Button>
+        <Button @click="limpiarPoligono">Limpiar zona</Button>
+      </ButtonGroup>
+
+      <br>
+      <br>
+      <h2>Crear zona:</h2>
+      <label for="nombreZona">Nombre:</label>
+      <input type="text" v-model="nombreZona" style="margin-right: 10px; height: 35px;"> </input>
+      <ButtonGroup>
+        <span v-if="3 < puntos.length">
+          <Button @click="crearPoligono">Mostrar poligono</Button>
+          <Button @click="crearZona">Crear zona</Button>
+        </span>
+      </ButtonGroup>
+    </div>
+    <br>
 
     <!-- Mapa -->
     <div id="map" style="height: 500px;"></div>
-		<br>
-		<div v-if="repartidores.length" class="map-container">
-			<h2>Repartidores {{ zonaSeleccionada.nombre }}</h2>
-			<div v-for="repartidor in repartidores">
-				{{ repartidor.nombre }}
-			</div>
-		</div>
+    <br>
+    <div v-if="repartidores.length" class="map-container">
+      <h2>Repartidores {{ zonaSeleccionada.nombre }}</h2>
+      <div v-for="repartidor in repartidores">
+        {{ repartidor.nombre }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -72,29 +72,29 @@ import clientService from '../services/client.service';
 import ubicacionIcon from '@/assets/ubicacion.png';
 import ubicacionPuntoMedio from '@/assets/ubicacion_pm.png';
 import zonaService from '@/services/zona.service';
-import Button  from 'primevue/button';
+import Button from 'primevue/button';
 import ButtonGroup from 'primevue/buttongroup';
 import Select from 'primevue/select';
 
 export default {
   name: 'MapView',
-	components:{
-		Button,
-		ButtonGroup,
-		Select
-	},
+  components: {
+    Button,
+    ButtonGroup,
+    Select
+  },
   data() {
     return {
       clientes: [],
       client1Id: null,
       client2Id: null,
       map: null,
-			puntos: [],
-			poligono: [],
-			nombreZona: null,
-			zonas: [],
-			repartidores: [],
-			zonaSeleccionada: null,
+      puntos: [],
+      poligono: [],
+      nombreZona: null,
+      zonas: [],
+      repartidores: [],
+      zonaSeleccionada: null,
       midpointMarker: null,
       client1Marker: null,
       client2Marker: null,
@@ -144,6 +144,9 @@ export default {
         const client1Coords = [client1.latitud, client1.longitud];
         const client2Coords = [client2.latitud, client2.longitud];
 
+        console.log('Coordenadas cliente 1:', client1Coords);
+        console.log('Coordenadas cliente 2:', client2Coords);
+
         if (!this.client1Marker) {
           this.client1Marker = L.marker(client1Coords, { icon: this.client1Icon })
             .addTo(this.map)
@@ -191,7 +194,7 @@ export default {
         alert('Ocurrió un error al calcular el punto medio.');
       }
     },
-		async cargarZonas() {
+    async cargarZonas() {
       try {
         const response = await zonaService.getByEstado("Disponible");
         this.zonas = response.data;
@@ -199,129 +202,133 @@ export default {
         console.error("Error al cargar las zonas:", error);
       }
     },
-		mostrarZona(zona){
-			console.log(zona)
-			function wktToLeafletCoordinates(wkt) {
-			const coordinatesText = wkt.match(/\(\(([^)]+)\)\)/)[1]; // Extraer el texto entre (())
-			const coordinatePairs = coordinatesText.split(','); // Separar las coordenadas
-			const leafletCoordinates = coordinatePairs.map(pair => {
-				const [lng, lat] = pair.trim().split(' ').map(Number);
-				return [lat, lng]; // Leaflet usa [lat, lng]
-			});
-			return leafletCoordinates;
-		}
-			if (this.poligono) this.map.removeLayer(this.poligono);
-			const polygonCoordinates = wktToLeafletCoordinates(zona.area); // Convertir WKT a coordenadas Leaflet
-			const polygon = L.polygon(polygonCoordinates, {
-				color: 'blue',               // Color del borde
-				fillColor: 'lightblue',        // Color de fondo
-				fillOpacity: 0.5            // Opacidad del relleno
-			}).addTo(this.map);
-
-			this.poligono = polygon
-
-			this.map.fitBounds(this.poligono.getBounds());
-
-			// 6️⃣ Agregar un popup al hacer clic en el polígono
-			polygon.bindPopup(`<b>${zona.nombre}</b><br>Estado: ${zona.estado}`);
-
-			},
-		async buscarRepartidores(){
-			try{
-				const response = await zonaService.findRepartidores(this.zonaSeleccionada.id)
-				this.repartidores = response.data
-				console.log(response.data)
-			}catch(error){
-				console.log(error)
-			}
-
-		},
-		crearPoligono() {
-		if (this.puntos.length >= 3) {
-			if (this.poligono) this.map.removeLayer(this.poligono);
-			this.poligono = L.polygon(this.puntos, {
-				color: 'blue',
-				fillColor: 'cyan',
-				fillOpacity: 0.5
-			}).addTo(this.map);
-			this.map.fitBounds(this.poligono.getBounds());
-			this.map.eachLayer(layer => {
-			if (layer instanceof L.Marker) {
-				this.map.removeLayer(layer);
-			}})
-		} else {
-			alert('Se necesitan al menos 3 puntos para crear un polígono.');
-		}
-	},
-	limpiarPoligono(){
-		if (this.poligono) this.map.removeLayer(this.poligono);
-		this.poligono = []
-		this.puntos = []
-
-		this.map.eachLayer(layer => {
-    if (layer instanceof L.Marker) {
-      this.map.removeLayer(layer);
-    }
-  });
-	},
-	async crearZona(){
-		function convertirACoordenadasWKT(coordenadas ) {
-			if (!Array.isArray(coordenadas) || coordenadas.length === 0) {
-				throw new Error('El arreglo de coordenadas es inválido o está vacío');
-			}
-				// Asegurarnos de que el polígono esté cerrado (el primer y último punto deben coincidir)
-				const primerPunto = coordenadas[0];
-				const ultimoPunto = coordenadas[coordenadas.length - 1];
-				if (primerPunto[0] !== ultimoPunto[0] || primerPunto[1] !== ultimoPunto[1]) {
-					coordenadas.push(primerPunto); // Cierra el polígono automáticamente
-				}
-				const coordenadasTexto = coordenadas.map(([lng, lat]) => `${lat} ${lng}`).join(', ');
-				return `SRID=4326;POLYGON((${coordenadasTexto}))`;
-			
-
-			throw new Error('Tipo de WKT no soportado. Usa POINT, LINESTRING o POLYGON');
-		}
-
-		console.log(this.puntos.length)
-
-		const areaZona = convertirACoordenadasWKT(this.puntos)
-
-		const zona = {
-			nombre: this.nombreZona,
-			estado: "Disponible",
-			area: convertirACoordenadasWKT(this.puntos)
-		}
-
-		console.log(areaZona)
-
-		await zonaService.create(zona)
-
-		this.cargarZonas()
-
-	}
-  },
-    mounted() {
-      // Inicializar mapa
-      this.map = L.map('map').setView([0, 0], 2);
-  
-      // Añadir capa base de OpenStreetMap
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    mostrarZona(zona) {
+      console.log(zona)
+      function wktToLeafletCoordinates(wkt) {
+        const coordinatesText = wkt.match(/\(\(([^)]+)\)\)/)[1]; // Extraer el texto entre (())
+        const coordinatePairs = coordinatesText.split(','); // Separar las coordenadas
+        const leafletCoordinates = coordinatePairs.map(pair => {
+          const [lng, lat] = pair.trim().split(' ').map(Number);
+          return [lat, lng]; // Leaflet usa [lat, lng]
+        });
+        return leafletCoordinates;
+      }
+      if (this.poligono) this.map.removeLayer(this.poligono);
+      const polygonCoordinates = wktToLeafletCoordinates(zona.area); // Convertir WKT a coordenadas Leaflet
+      const polygon = L.polygon(polygonCoordinates, {
+        color: 'blue',               // Color del borde
+        fillColor: 'lightblue',        // Color de fondo
+        fillOpacity: 0.5            // Opacidad del relleno
       }).addTo(this.map);
 
-			this.map.on('click', (e) => {
-				const { lat, lng } = e.latlng; // Obtener latitud y longitud del clic
-				this.puntos.push([lat, lng]); // Agregar el punto a la lista
-				L.marker([lat, lng]).addTo(this.map) // Mostrar un marcador en la posición clicada
-					.bindPopup(`Punto ${this.puntos.length}: [${lat.toFixed(5)}, ${lng.toFixed(5)}]`)
-					.openPopup();
-			});
-  
-      // Obtener la lista de clientes al cargar el componente
-      this.fetchClientes();
-			this.cargarZonas();
+      this.poligono = polygon
+
+      this.map.fitBounds(this.poligono.getBounds());
+
+      // 6️⃣ Agregar un popup al hacer clic en el polígono
+      polygon.bindPopup(`<b>${zona.nombre}</b><br>Estado: ${zona.estado}`);
+
     },
-  };
+    async buscarRepartidores() {
+      try {
+        const response = await zonaService.findRepartidores(this.zonaSeleccionada.id)
+        this.repartidores = response.data
+        console.log(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+
+    },
+    crearPoligono() {
+      if (this.puntos.length >= 3) {
+        if (this.poligono) this.map.removeLayer(this.poligono);
+        this.poligono = L.polygon(this.puntos, {
+          color: 'blue',
+          fillColor: 'cyan',
+          fillOpacity: 0.5
+        }).addTo(this.map);
+        this.map.fitBounds(this.poligono.getBounds());
+        this.map.eachLayer(layer => {
+          if (layer instanceof L.Marker) {
+            this.map.removeLayer(layer);
+          }
+        })
+      } else {
+        alert('Se necesitan al menos 3 puntos para crear un polígono.');
+      }
+    },
+    limpiarPoligono() {
+      if (this.poligono) this.map.removeLayer(this.poligono);
+      this.poligono = []
+      this.puntos = []
+      this.midpointMarker = null
+      this.client1Marker = null
+      this.client2Marker = null
+
+      this.map.eachLayer(layer => {
+        if (layer instanceof L.Marker) {
+          this.map.removeLayer(layer);
+        }
+      });
+    },
+    async crearZona() {
+      function convertirACoordenadasWKT(coordenadas) {
+        if (!Array.isArray(coordenadas) || coordenadas.length === 0) {
+          throw new Error('El arreglo de coordenadas es inválido o está vacío');
+        }
+        // Asegurarnos de que el polígono esté cerrado (el primer y último punto deben coincidir)
+        const primerPunto = coordenadas[0];
+        const ultimoPunto = coordenadas[coordenadas.length - 1];
+        if (primerPunto[0] !== ultimoPunto[0] || primerPunto[1] !== ultimoPunto[1]) {
+          coordenadas.push(primerPunto); // Cierra el polígono automáticamente
+        }
+        const coordenadasTexto = coordenadas.map(([lng, lat]) => `${lat} ${lng}`).join(', ');
+        return `SRID=4326;POLYGON((${coordenadasTexto}))`;
+
+
+        throw new Error('Tipo de WKT no soportado. Usa POINT, LINESTRING o POLYGON');
+      }
+
+      console.log(this.puntos.length)
+
+      const areaZona = convertirACoordenadasWKT(this.puntos)
+
+      const zona = {
+        nombre: this.nombreZona,
+        estado: "Disponible",
+        area: convertirACoordenadasWKT(this.puntos)
+      }
+
+      console.log(areaZona)
+
+      await zonaService.create(zona)
+
+      this.cargarZonas()
+
+    }
+  },
+  mounted() {
+    // Inicializar mapa
+    this.map = L.map('map').setView([0, 0], 2);
+
+    // Añadir capa base de OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
+
+    this.map.on('click', (e) => {
+      const { lat, lng } = e.latlng; // Obtener latitud y longitud del clic
+      this.puntos.push([lat, lng]); // Agregar el punto a la lista
+      L.marker([lat, lng]).addTo(this.map) // Mostrar un marcador en la posición clicada
+        .bindPopup(`Punto ${this.puntos.length}: [${lat.toFixed(5)}, ${lng.toFixed(5)}]`)
+        .openPopup();
+    });
+
+    // Obtener la lista de clientes al cargar el componente
+    this.fetchClientes();
+    this.cargarZonas();
+  },
+};
 </script>
 
 <style>
